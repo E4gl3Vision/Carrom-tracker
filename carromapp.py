e21251483a0b513623d0887a24dd72027478637d
E4gl3Vision <102818922+E4gl3Vision@users.noreply.github.com>
2026-07-14 19:15:22 +0530

    Update carromapp.py

--- carromapp.py
+++ carromapp.py
@@ -40,7 +40,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 st.title("🎯 4-Player Club Carrom Tracker")
-st.caption("Live multi-player dashboard with Net Coin tracking and Shot Frequencies.")
+st.caption("Live multi-player rating dashboard.")
 
 # --- DATA DICTIONARIES ---
 multipliers = {
